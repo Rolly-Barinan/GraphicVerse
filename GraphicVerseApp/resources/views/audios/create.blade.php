45685e3d2f5caef4613a31882eea7b4f33977d80
Rairu <lylekevin.tamarra.21@usjr.edu.ph>
2023-08-01 15:48:43 +0800

    modified audio form

--- GraphicVerseApp/resources/views/audios/create.blade.php
+++ GraphicVerseApp/resources/views/audios/create.blade.php
@@ -1,38 +1,53 @@
 @extends('layouts.app')
+
 @section('content')
-    <h1>Upload Audio</h1>
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Upload Audio</h1>
+                </div>
 
-    <form method="POST" action="{{ route('audios.store') }}" enctype="multipart/form-data">
-        @csrf
+                <div class="card-body">
+                    <form method="POST" action="{{ route('audios.store') }}" enctype="multipart/form-data">
+                        @csrf
 
-        <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name" class="form-control">
-            @error('name')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror">
+                            @error('name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="category">Category:</label>
+                            <select name="category" id="category" class="form-control @error('category') is-invalid @enderror">
+                                <option value="music">Music</option>
+                                <option value="sound">Sound</option>
+                                <option value="ambient">Ambient</option>
+                            </select>
+                            @error('category')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="audio">Select Audio:</label>
+                            <input type="file" name="audio[]" id="audio" class="form-control-file @error('audio') is-invalid @enderror" multiple>
+                            @error('audio')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <button type="submit" class="btn btn-primary">Upload</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div class="form-group">
-            <label for="category">Category:</label>
-            <select name="category" id="category" class="form-control">
-                <option value="music">Music</option>
-                <option value="sound">Sound</option>
-                <option value="ambient">Ambient</option>
-            </select>
-            @error('category')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <div class="form-group">
-            <label for="audio">Select Audio:</label>
-            <input type="file" name="audio[]" id="audio" class="form-control" multiple>
-            @error('audio')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <button type="submit" class="btn btn-primary">Upload</button>
-    </form>
+    </div>
+</div>
 @endsection
