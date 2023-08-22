4bba89161cfd604107c025fb6cc91c5f3cc656ec
Rairu <lylekevin.tamarra.21@usjr.edu.ph>
2023-08-22 22:11:18 +0800

    adjust css for show detail of 3d asset

--- GraphicVerseApp/resources/views/three-dim/show.blade.php
+++ GraphicVerseApp/resources/views/three-dim/show.blade.php
@@ -35,7 +35,7 @@
     </head>
 
     <body>
-        <div class="container">
+        <div class="container mt-4">
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
