fe9e8a1c17db5c8c6e450cf8c885e56f99dff431
Rhodmar <gems.rabaca@gmail.com>
2023-08-29 09:08:23 +0800

    Navbar re-fixed

--- GraphicVerseApp/resources/views/layouts/app.blade.php
+++ GraphicVerseApp/resources/views/layouts/app.blade.php
@@ -44,107 +44,93 @@
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 </head>
 
-<style>
-    body {
-        font-family: 'Nunito', sans-serif;
-        background-image: url('https://cdn.discordapp.com/attachments/1121006331323760680/1123571308496691210/Copy_of_GraphicVerse_Capstone_Hearing.png');
-        background-size: cover;
-        background-repeat: no-repeat;
-    }
-
-    .navbar-nav .nav-item {
-        display: flex;
-    }
-</style>
 
 <nav class="navbar-fluid navbar-expand-lg shadow">
-    <div class="container-fluid pt-2">
-        <div class="row ">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="row ">
 
 
-            <div class="col" style="display: flex; align-items: center;" href="{{ url('/') }}">
-                <img src="/svg/GraphicVerse_Capstone.png" class="pr-3" style="height: 40px; margin-left: 4rem;">
-                <a class="GraphicVerse navbar-brand text-white" style="margin-left: 10px;">
-                    GraphicVerse
-                </a>
-            </div>
+                <div class="col" style="display: flex; align-items: center;" href="{{ url('/') }}">
+                    <img src="/svg/GraphicVerse_Logo.png" class="pr-3" style="height: 50px; width: 150px margin-top: .5rem;">
+                </div>
 
-            <div class="col " style="margin-top: 10px">
-                <form action="{{ route('search') }}" method="get" class="d-flex custom-search-form">
-                    <input class="form-control me-2 custom-search-input" type="search" name="q" placeholder="&#128269; Search assets" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>                                
-            </div>
+                <div class="col " style="margin-top: 10px">
+                    <form class="d-flex custom-search-form" role="search">
+                        <input class="form-control me-2 custom-search-input" type="search" placeholder=" &#128269; "
+                            aria-label="Search">
+                    </form>
+                </div>
 
 
-            <div class="col" style="margin-top: 10px">
-                {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> --}}
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active text-white pe-4" aria-current="page" href="/home">HOME</a>
-                        <a class="nav-link text-white pe-4" href="/aboutus">ABOUT US</a>
-                        <a class="nav-link text-white pe-4" href="/community">COMMUNITY</a>
-                    </li>
-                </ul>
-                {{-- </div> --}}
-            </div>
-            <div class="mx-auto col-auto ps-5" style="margin-top: 10px">
-                <ul class="navbar-nav ms-auto ps-5">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item pe-2">
-                                <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
+                <div class="col" style="margin-top: 10px">
+                    {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> --}}
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active text-white pe-4" aria-current="page" href="/home">HOME</a>
+                            <a class="nav-link text-white pe-4" href="/aboutus">ABOUT US</a>
+                            <a class="nav-link text-white pe-4" href="/community">COMMUNITY</a>
+                        </li>
+                    </ul>
+                    {{-- </div> --}}
+                </div>
+                <div class="mx-auto col-auto ps-5">
+                    <ul class="navbar-nav ms-auto ps-5">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item pe-2">
+                                    <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
 
-                        @if (Route::has('register'))
-                            <li class="nav-item pe-2">
-                                <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <div class="dropdown">
-                            <button class="btn btn-secondary btn-lg" type="button" data-bs-toggle="dropdown"
-                                style="background-color: #499096;  border-bottom-right-radius: 25px;
-                                border-bottom-left-radius: 25px; "
-                                aria-expanded="false">
-                                <div class="row" style="  text-align: center;">
-                                    <div class="col"> {{ Auth::user()->name }}</div>
-                                    <div class="col">
-                                        <img src="/svg/GraphicVerse_Capstone.png" class="" style="height: 40px;">
+                            @if (Route::has('register'))
+                                <li class="nav-item pe-2">
+                                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <div class="dropdown">
+                                <button class="dropdown-button" type="button" data-bs-toggle="dropdown">
+                                    <div class="row1">
+                                        <div class="col"> <h5 class="name">{{ Auth::user()->profile->title }}</h5>
+                                            <h5 class="credits">P 130.49</h5>
+                                        </div>
+                                        <div class="col">
+                                            <img src="{{ Auth::user()->profile->profileImage() }}" class="" style="height: 50px; border-radius:100%; margin-right: 25px;">
+                                        </div>
                                     </div>
-                                </div>
 
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/profile/{{ Auth::user()->id }}">My Profile</a></li>
-                                <li><a class="dropdown-item" href="#">My Portfolio</a></li>
-                                <li><a class="dropdown-item" href="#">Wishlist</a></li>
-                                <li><a class="dropdown-item" href="/teams">Teams</a></li>
-                                <li> <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a></li>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/profile/{{ Auth::user()->id }}">My Profile</a></li>
+                                    <li><a class="dropdown-item" href="#">Wishlist</a></li>
+                                    <li><a class="dropdown-item" href="/teams">Teams</a></li>
+                                    <li> <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a></li>
 
-                            </ul>
-                        </div>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
+                                </ul>
+                            </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
 
 
-                    @endguest
-                </ul>
-            </div>
+                        @endguest
+                    </ul>
+                </div>
 
             <div class="row justify-content-center navTwo">
                 <div class="col-auto navbar-nav ">
                     <div class="d-flex justify-content-center space-between">
                         <a class="twod nav-link text-white pe-5" href="/2d">2D</a>
-                        <a class="threed nav-link text-white pe-5" href="/3d">3D</a>
+                        <a class="threed nav-link text-white pe-5" href="3d">3D</a>
                         <a class="others nav-link text-white pe-5  " href="animation">Animation</a>
                         <a class="others nav-link text-white pe-5  " href="music">Sound/Music</a>
+
+                        </div>
                     </div>
                 </div>
             </div>
