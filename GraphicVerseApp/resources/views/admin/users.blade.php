ead0c573d21f21b7ba5c027b56b85737766dbbd2
Rairu <lylekevin.tamarra.21@usjr.edu.ph>
2023-10-08 23:34:50 +0800

    modified user list to look like the categories list

--- GraphicVerseApp/resources/views/admin/users.blade.php
+++ GraphicVerseApp/resources/views/admin/users.blade.php
@@ -2,10 +2,10 @@
 
 @section('admin-content')
 <body>
-    <div class="container mt-2">
+    <div class="container mt-4">
         <div class="row">
             <div class="col-lg-12 margin-tb">
-                <div class="pull-right mb-2">
+                <div class="pull-right mb-4">
                     <h2>Users</h2>
                 </div>
             </div>
@@ -16,22 +16,41 @@
             </div>
         @endif
 
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                    <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->username }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="card rounded" style="border-left: 10px solid #333;">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-borderless">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Date Created</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->id }}</td>
+                                    <td>{{ $user->username }}</td>
+                                    <td>{{ $user->created_at->format('Y-m-d') }}</td>
+                                    <td class="text-center">
+                                        <a href="{{ route('admin.deleteCategory', $user->id) }}" class="btn btn-success">
+                                            View Details
+                                        </a>                                        
+                                        <span style="margin: 0 10px;"> <!-- Add space between icons -->
+                                            <a href="{{ route('admin.deleteCategory', $user->id) }}" onclick="return confirm('Are you sure you want to delete this category?')" style="text-decoration: none; color: red;">
+                                                <i class="fas fa-trash-alt"></i> <!-- Font Awesome delete icon -->
+                                            </a>
+                                        </span>
+                                    </td>  
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 
         <!-- Pagination Links -->
         <div class="d-flex justify-content-center">
